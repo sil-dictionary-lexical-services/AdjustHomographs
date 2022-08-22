136efda79262a809a00e16e47275eed457355040
Wes <wes_peacock@sil.org>
2022-08-22 05:55:29 -0600

    Add wrapper script for AdjstHm.pl & FlagseREF.pl
     * default SFM file is adjust.sfm

---
+++ runAdjHm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# call this like:
+#    dbname=myfile.sfm ./runAdjHm.sh
+
+# The next variables should be set before the script is run
+# if it's not, a default is used
+dbname="${dbname:-adjust.sfm}"
+
+bkpdbname="${dbname%.*}.bkp" # replace extension with bkp
+refdbname="${dbname%.*}.ref" # replace extension with ref
+echo $dbname $bkpdbname $refdbname
+cp $dbname $bkpdbname
+perl -pf opl.pl $bkpdbname |./FlagseReF.pl |perl -pf de_opl.pl |tee $refdbname |./AdjstHm.pl |perl -pE 's/^\\REF/\\/' >$dbname
