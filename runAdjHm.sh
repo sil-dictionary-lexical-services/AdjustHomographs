35f5c04236f534fa7395b4ce78c8ba587e25ff26
bbryson <Beth-docs_Bryson@sil.org>
2023-10-25 19:34:31 -0500

    Usability adjustments
    Call with input and output filenames.
    For ancillary files, put modifiers in the basename of the file, not the extension.
    Report the filenames for Log and Err that the script will create.
    Use the new versions of oplsfm.pl and de_oplsfm.pl.
    Delete the Err file if it is empty.

--- runAdjHm.sh
+++ runAdjHm.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+
 # call this like:
-#    dbname=myfile.sfm ./runAdjHm.sh
+#    ./runAdjHm.sh infile.db outfile.db
 
-# The next variables should be set before the script is run
-# if it's not, a default is used
-dbname="${dbname:-adjust.sfm}"
+# Collect input/output filenames from command line
+infilename=$1
+outfilename=$2
+# The name of the main script, to use for constructing filenames
+scriptfilename="AdjstHm"
+# Construct ancillary filenames
+refdbname="${infilename%.*}-ref.txt" # create FlagRef filename
+# This is what the script AdjstHm.pl is currently doing:
+logfilename="$scriptfilename-log.txt" # create Log filename
+errfilename="$scriptfilename-err.txt" # create Log filename
+## If we want to construct the log/err files from the input filename
+## (but we would have to pass them to the script)
+#logfilename="${infilename%.*}-log.txt" # create Log filename
+#errfilename="${infilename%.*}-err.txt" # create Log filename
 
-bkpdbname="${dbname%.*}.bkp" # replace extension with bkp
-refdbname="${dbname%.*}.ref" # replace extension with ref
-echo "Original:$bkpdbname Reference Flagged:$refdbname Modified:$dbname"
-cp $dbname $bkpdbname
-perl -pf opl.pl $bkpdbname |./FlagseReF.pl |perl -pf de_opl.pl |tee $refdbname |./AdjstHm.pl |perl -pE 's/^\\REF/\\/' >$dbname
+# Report the filenames we're using
+echo "Original:$infilename "
+echo "Flagged Refs File:$refdbname"
+echo "LogFile:$logfilename  ErrFile:$errfilename "
+echo "Modified:$outfilename"
+
+# Call the sequence of commands to run the operation
+./oplsfm.pl < $infilename | ./FlagseReF.pl | ./de_oplsfm.pl |tee $refdbname | ./AdjstHm.pl | perl -pE 's/^\\REF/\\/' >$outfilename
+
+# If the error file is empty, delete it
+if ! [ -s "AdjstHm-err.txt" ];then
+    #echo "Deleting empty Error file AdjstHm-err.txt"
+	rm AdjstHm-err.txt
+fi
+
